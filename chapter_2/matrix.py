afa413f63e3f3bd6fb7b46c5ea98c39758f7796a
Gowtham Behara <gothsiddu@gmail.com>
2019-11-06 16:19:05 +0530

    Define funcs to get specific row and col from matrix

--- chapter_2/matrix.py
+++ chapter_2/matrix.py
@@ -1,6 +1,12 @@
 def shape(matrix): return len(matrix), len(matrix[0])
 
 
+def get_row(matrix, row_idx): return matrix[row_idx]
+
+
+def get_col(matrix, col_idx): return [matrix[row_idx][col_idx] for row_idx in matrix]
+
+
 def main():
     example_matrix = [[1, 2, 3], [4, 5, 6]]
     print(shape(example_matrix))
