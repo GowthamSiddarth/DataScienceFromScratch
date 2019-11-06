c4c3f16db34ef61f802025475584774904eac812
Gowtham Behara <gothsiddu@gmail.com>
2019-11-06 16:49:00 +0530

    Redefine func to emit identity elem and construct identity matrix

--- chapter_2/matrix.py
+++ chapter_2/matrix.py
@@ -11,12 +11,16 @@ def make_matrix(num_of_rows, num_of_cols, generate_elem):
     return [[generate_elem(row_idx, col_idx) for col_idx in range(num_of_cols)] for row_idx in range(num_of_rows)]
 
 
-def is_diagonal_elem(row_idx, col_idx, elem): return elem if row_idx == col_idx else 0
+def is_identity_elem(row_idx, col_idx): return 1 if row_idx == col_idx else 0
 
 
 def main():
     example_matrix = [[1, 2, 3], [4, 5, 6]]
     print(shape(example_matrix))
+
+    num_of_rows, num_of_cols = 5, 5
+    identity_matrix = make_matrix(num_of_rows, num_of_cols, is_identity_elem)
+    print(identity_matrix)
 
 
 if __name__ == '__main__':
