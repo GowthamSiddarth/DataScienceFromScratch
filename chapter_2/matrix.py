8f0b91c00024b7981d47e566d03879acc72e527b
Gowtham Behara <gothsiddu@gmail.com>
2019-11-06 16:22:46 +0530

    Define func to construct matrix with dim and generator func

--- chapter_2/matrix.py
+++ chapter_2/matrix.py
@@ -7,6 +7,10 @@ def get_row(matrix, row_idx): return matrix[row_idx]
 def get_col(matrix, col_idx): return [matrix[row_idx][col_idx] for row_idx in matrix]
 
 
+def make_matrix(num_of_rows, num_of_cols, generate_elem):
+    return [[generate_elem(row_idx, col_idx) for col_idx in range(num_of_cols)] for row_idx in range(num_of_rows)]
+
+
 def main():
     example_matrix = [[1, 2, 3], [4, 5, 6]]
     print(shape(example_matrix))
