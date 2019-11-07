5660f3e3ecb96d7d4c5a77ce9c8b63f9e191aa9e
Gowtham Behara <gothsiddu@gmail.com>
2019-11-07 10:38:21 +0530

    Define func to compute mean num of friends; Use draw before show to continue execution after plotting

--- chapter_5/describe_single_set_data.py
+++ chapter_5/describe_single_set_data.py
@@ -12,14 +12,21 @@ def plot_friends_vs_people(num_of_friends, num_of_people):
     plt.title("Histogram of friends count")
     plt.xlabel("# of friends")
     plt.ylabel("# of people")
-    plt.show()
+    plt.draw()
+
+
+def mean(num_of_friends): return sum(num_of_friends) / len(num_of_friends)
 
 
 def main():
     random.seed(42)
-    num_of_friends = [random.randint(1, 100) for _ in range(100)]
+    num_of_friends = [random.randint(1, 204) for _ in range(100)]
     num_of_people = range(101)
     plot_friends_vs_people(num_of_friends, num_of_people)
+
+    mean_num_of_friends = mean(num_of_friends)
+    print(mean_num_of_friends)
+    plt.show()
 
 
 if __name__ == '__main__':
