176aaa1ee7cb829e08e19cc8f7fc9df8fff77da1
Gowtham Behara <gothsiddu@gmail.com>
2019-11-07 15:06:41 +0530

    Define func to compute correlation

--- chapter_5/correlation.py
+++ chapter_5/correlation.py
@@ -1,9 +1,14 @@
 import random
 from chapter_4.vector import dot_product
-from chapter_5.describe_single_set_data import dev_from_mean
+from chapter_5.describe_single_set_data import dev_from_mean, standard_deviation
 
 
 def covariance(x, y): return dot_product([dev_from_mean(x), dev_from_mean(y)]) / (len(x) - 1)
+
+
+def correlation(x, y):
+    std_dev_x, std_dev_y = standard_deviation(x), standard_deviation(y)
+    return covariance(x, y) / std_dev_x / std_dev_y if std_dev_x > 0 and std_dev_y > 0 else 0
 
 
 def main():
@@ -14,6 +19,9 @@ def main():
 
     cov_friends_minutes = covariance(num_of_friends, daily_minutes)
     print(cov_friends_minutes)
+
+    corr_friends_minutes = correlation(num_of_friends, daily_minutes)
+    print(corr_friends_minutes)
 
 
 if __name__ == '__main__':
